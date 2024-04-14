850cdd573b52c91dd286d1c619e20e9044a521ca
Isaac Culver <idculver27@gmail.com>
2024-04-13 23:43:37 -0500

    he speaks!

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
-const { Client } = require("discord.js");
+const { Client, GatewayIntentBits } = require("discord.js");
 const { token } = require("./config.json");
 
-const client = new Client({intents: []});
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildScheduledEvents
+    ]
+});
+
+client.on("ready", (c) => {
+    console.log(c.user.tag + " is ready");
+});
+
+client.on("messageCreate", (msg) => {
+    if(msg.author.bot) return
+    
+    console.log(msg);
+    msg.channel.send("hello world");
+});
 
 client.login(token);
-
-console.log("hello world");
